b5f72ebbf6cabe82b6b9d5b0d0eb46634bdf4ad7
kasahun welela <getasewmequanint@cbe.com.et>
2023-07-17 14:52:06 +0300

    footer page created

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="bg-gray-900 text-gray-200 text-center mt-2 py-6">
+      <h3 className="font-bold">My Learning Journal</h3>
+      <p>copyright@2023</p>
+    </div>
+  );
+}
+
+export default Footer;
