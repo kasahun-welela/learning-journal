e9d0dc3f914ef80028250a278b7ef187f3eeb9dd
kasahun welela <getasewmequanint@cbe.com.et>
2023-07-17 16:56:18 +0300

    margin and padding updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="bg-gray-900 text-gray-200 text-center mt-2 py-6">
+    <div className="bg-gray-900 text-gray-200 text-center mt-14 py-10">
       <h3 className="font-bold">My Learning Journal</h3>
       <p>copyright@2023</p>
     </div>
