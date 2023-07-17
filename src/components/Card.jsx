e4446fde4d2e3d745d827f6ddd66c63222c48b51
kasahun welela <getasewmequanint@cbe.com.et>
2023-07-17 10:56:36 +0300

    card created

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 
-function Card() {
+function Card({ src, dateOfPost, title, description }) {
   return (
-    <div className="border-2 border-x-yellow-900 mt-6 p-3 flex flex-col gap-2">
-      <img src="./blog-image-06.png" className="" alt="card image" />
-      <p className="text-gray-600">JULY 23, 2023</p>
-      <h3 className="text-xl font-bold">Blog one</h3>
-      <p className="text-gray-600">
-        I'm excited to start a new learning journey as a Scrimba Bootcamp
-        student! After several months of learning in the Frontend Developer
-        Career Path.
-      </p>
-    </div>
+    <>
+      <div className="border-2 border-yellow-900 mt-6 p-3 flex flex-col gap-2">
+        <img src={src} className="" alt="card image" />
+        <p className="text-gray-600">{dateOfPost}</p>
+        <h3 className="text-xl font-bold">{title}</h3>
+        <p className="text-gray-600">{description}</p>
+      </div>
+    </>
   );
 }
 
