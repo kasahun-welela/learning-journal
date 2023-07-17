629ef84c6cfe5e21f49aa085e1cd109c8f251c03
kasahun welela <getasewmequanint@cbe.com.et>
2023-07-17 14:38:25 +0300

    scaled on hover

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Card({ src, dateOfPost, title, description }) {
   return (
     <>
-      <div className="border-2 border-yellow-900 mt-6 p-3 flex flex-col gap-2">
+      <div className="hover:scale-105  mt-6 p-3 flex flex-col gap-2 max-w-md">
         <img src={src} className="" alt="card image" />
         <p className="text-gray-600">{dateOfPost}</p>
         <h3 className="text-xl font-bold">{title}</h3>
