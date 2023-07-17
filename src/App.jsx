96cdfa3dc9dd624e1fcb517ab3cf2049cf0c41bb
kasahun welela <getasewmequanint@cbe.com.et>
2023-07-17 10:58:22 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,12 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import NavBar from "./components/NavBar";
-import Hero from "./components/Hero";
-import Card from "./components/Card";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <NavBar />
-      <Hero />
-      <Card />
+      <Home />
     </>
   );
 }
