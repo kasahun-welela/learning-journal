40fddf8f1774454aabc1bb6a442290553bf11d96
kasahun welela <getasewmequanint@cbe.com.et>
2023-07-17 14:51:41 +0300

    size updated for MD

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,14 +6,14 @@ function Hero() {
       <img
         src="./article-image-hero.png"
         alt="hero image"
-        className="w-full relative"
+        className="w-screen relative md:h-[600px]"
       />
-      <div className="absolute bottom-0 right-0 text-white p-3">
+      <div className="absolute bottom-0 left-0 text-white p-3 md:w-1/3 md:ml-32">
         <p>JULY 23, 2022</p>
-        <h1 className="text-3xl font-bold">
+        <h1 className="text-2xl md:text-4xl font-bold">
           My new journey as a bootcamp student.
         </h1>
-        <p>
+        <p className="md:text-xl">
           After several months of learning in the Frontend Developer Career
           Path, I've made the big jump over to the Bootcamp to get expert code
           reviews of my Solo Projects projects and meet like-minded peers.
