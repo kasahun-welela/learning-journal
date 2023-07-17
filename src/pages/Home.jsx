2510f1a211af6fd5747156c00282482d846ec6d2
kasahun welela <getasewmequanint@cbe.com.et>
2023-07-17 16:54:52 +0300

    page responsiveness for media query done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,22 +4,26 @@ import Hero from "../components/Hero";
 
 import data from "../datas";
 import Card from "../components/Card";
+import Footer from "../components/Footer";
 
 function Home() {
   return (
     <>
       <NavBar />
       <Hero />
+      <div className="md:grid grid-cols-3 gap-2 md:w-3/4 mx-auto ">
+        {data.map((value) => (
+          <Card
+            key={value.id}
+            title={value.title}
+            description={value.description}
+            dateOfPost={value.dateOfPost}
+            src={value.src}
+          />
+        ))}
+      </div>
 
-      {data.map((value) => (
-        <Card
-          key={value.id}
-          title={value.title}
-          description={value.description}
-          dateOfPost={value.dateOfPost}
-          src={value.src}
-        />
-      ))}
+      <Footer />
     </>
   );
 }
