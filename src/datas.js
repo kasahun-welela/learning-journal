f6f24eeb5c597db75c57474582e7c12e4ad0abe8
kasahun welela <getasewmequanint@cbe.com.et>
2023-07-17 10:57:17 +0300

    create data

---
+++ src/datas.js
@@ -0,0 +1,51 @@
+const data = [
+  {
+    id: 1,
+    title: "Blog one",
+    description:
+      " I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    dateOfPost: "JULY 23, 2023",
+    src: "./blog-image-06.png",
+  },
+  {
+    id: 2,
+    title: "Blog two",
+    description:
+      " I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    dateOfPost: "JULY 23, 2023",
+    src: "./blog-image-04.png",
+  },
+  {
+    id: 3,
+    title: "Blog three",
+    description:
+      " I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    dateOfPost: "JULY 23, 2023",
+    src: "./blog-image-05.png",
+  },
+  {
+    id: 4,
+    title: "Blog four",
+    description:
+      " I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    dateOfPost: "JULY 23, 2023",
+    src: "./blog-image-03.png",
+  },
+  {
+    id: 5,
+    title: "Blog five",
+    description:
+      " I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    dateOfPost: "JULY 23, 2023",
+    src: "./blog-image-01.png",
+  },
+  {
+    id: 6,
+    title: "Blog six",
+    description:
+      " I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    dateOfPost: "JULY 23, 2023",
+    src: "./blog-image-02.png",
+  },
+];
+export default data;
