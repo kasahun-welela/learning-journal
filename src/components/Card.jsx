d06ba0b4818452984b1b039224502224a747cd9f
kasahun welela <getasewmequanint@cbe.com.et>
2023-07-18 15:45:07 +0300

    id and navigation added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 
-function Card({ src, dateOfPost, title, description }) {
+function Card({ src, dateOfPost, title, description, id }) {
   return (
     <>
-      <div className="hover:scale-105  mt-6 p-3 flex flex-col gap-2 max-w-md dark:bg-slate-900">
-        <img src={src} className="" alt="card image" />
-        <p className="text-gray-600 dark:text-slate-400">{dateOfPost}</p>
-        <h3 className="text-xl font-bold dark:text-white">{title}</h3>
-        <p className="text-gray-600 dark:text-slate-400">{description}</p>
-      </div>
+      <a href={`/details/${id}`}>
+        <div className="hover:scale-105  mt-6 p-3 flex flex-col gap-2 max-w-md dark:bg-slate-900">
+          <img src={src} className="" alt="card image" />
+          <p className="text-gray-600 dark:text-slate-400">{dateOfPost}</p>
+          <h3 className="text-xl font-bold dark:text-white">{title}</h3>
+          <p className="text-gray-600 dark:text-slate-400">{description}</p>
+        </div>
+      </a>
     </>
   );
 }
